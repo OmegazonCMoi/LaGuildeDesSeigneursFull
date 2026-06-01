68a0f9ded352b5c8bc55b360452af8beb8ef3aeb
Auria-dev <64524466+Auria-dev@users.noreply.github.com>
2026-06-01 12:02:09 +0200

    Login Register

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -10,10 +10,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/character')]
 final class CharacterController extends AbstractController
 {
+    public function __construct(
+        private SluggerInterface $slugger,
+    ) {
+    }
+
     #[Route(name: 'app_character_index', methods: ['GET'])]
     public function index(CharacterRepository $characterRepository): Response
     {
@@ -30,6 +36,11 @@ final class CharacterController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $character->setIdentifier(hash('sha1', uniqid()));
+            $character->setSlug($this->slugger->slug($character->getName())->lower());
+            $character->setCreation(new \DateTime());
+            $character->setModification(new \DateTime());
+
             $entityManager->persist($character);
             $entityManager->flush();
 
@@ -59,6 +70,9 @@ final class CharacterController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $character->setSlug($this->slugger->slug($character->getName())->lower());
+            $character->setModification(new \DateTime());
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_character_index', [], Response::HTTP_SEE_OTHER);
